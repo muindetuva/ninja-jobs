543d3191a72059d4d76ed1c09607beda5c93f4d2
Alfred Tuva <muindetuva@gmail.com>
2022-02-16 11:06:15 +0300

    Working with vue router

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>    
+    <p>Loermdjewpf jd fjqpdfj okldfjowkldjfoqklj</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,31 @@
+<template>
+	<div v-if="job">
+		<h1>{{ job.title }}</h1>
+		<p>Job id is {{ job.id }}</p>
+		<p>{{ job.details }}</p>	
+	</div>
+	<div v-else>
+		<p>Loading Job details ...</p>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			job: null 
+		}
+	},
+	props: ['id'],
+	mounted() {
+		fetch('http://localhost:3000/jobs/' + this.id)
+			.then(res => res.json())
+			.then(data => this.job = data)
+			.catch(err => console.log(err.message));		
+	}
+
+
+}
+</script>
+
+<style></style>
